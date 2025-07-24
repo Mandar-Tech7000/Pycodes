1fcabf5544057ae2b94e7d9aa5603d5a32d15962
Mandar-Tech7000 <mandarkumatkar10@gmail.com>
2025-07-25 00:38:07 +0530

    Create Calculater.py

---
+++ Calculater.py
@@ -0,0 +1,27 @@
+def calculator():
+    print("Simple Calculator")
+    
+    num1 = float(input("Enter first number: "))
+    operator = input("Enter operator (+, -, *, /): ")
+    num2 = float(input("Enter second number: "))
+
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            print("Error: Division by zero is not allowed.")
+            return
+    else:
+        print("Invalid operator.")
+        return
+
+    print("Result:", result)
+
+# Run the calculator
+calculator()
